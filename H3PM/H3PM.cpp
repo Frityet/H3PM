65c71690f294a3c95e10748a1249ce987b93e32a
SHK2DEV <rowblocks@gmail.com>
2021-03-19 20:25:21 -0400

    Added control structure for command line arguments (unfinished)

--- H3PM/H3PM.cpp
+++ H3PM/H3PM.cpp
@@ -7,8 +7,39 @@
 #include "JSON/ModListFormat.h"
 #include "JSON/SingularMods.h"
 
+// Map brevity codes to commands
+std::map<std::string, int> commands = {
+    { "list", 1 },
+    { "install", 2 },
+    { "remove", 3 },
+    { "help", 4 },
+};
+
 // Entry point
 int main(int argc, char* argv[])
 {
+    switch (commands[argv[1]]) {
+
+        // list
+        case 1:
+            return listMods(argc,argv);
+            break;
+        // install
+        case 2:
+            break;
+        // remove
+        case 3:
+
+            break;
+        // help
+        case 4:
+            break;
+    }
+}
+
+// Lists out the mods
+int listMods(int argc, char* argv[]) {
     // TODO
+    std::cout << "lol" << std::endl;
+    return 0; // Indicate success
 }
